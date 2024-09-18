43a35629d899028e41ce09bfd78eebc71bcaa6e4
Fabian Poels <fabian@fabianpoels.com>
2024-09-18 15:25:18 +0200

    cookies

--- src/main.js
+++ src/main.js
@@ -9,6 +9,19 @@ import Aura from '@primevue/themes/aura'
 import ToastService from 'primevue/toastservice'
 import Tooltip from 'primevue/tooltip'
 
+// cookies
+import { globalCookiesConfig } from 'vue3-cookies'
+const env = import.meta.env.VITE_ENV
+if (env === 'production') {
+  globalCookiesConfig({
+    expireTimes: '30d',
+    path: '/',
+    domain: 'fabianpoels.com',
+    secure: true,
+    sameSite: 'Strict'
+  })
+}
+
 import App from './App.vue'
 
 const app = createApp(App)
